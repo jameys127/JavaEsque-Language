36b19dbea7502766ea6673ac25c30b77e8d593ed
Jeremiah Sheehy <jameys127@gmail.com>
2025-04-22 00:28:53 -0700

    methodexp is going to be a nightmare. going to bed cuz i got work

--- src/main/java/com/jamey/compiler/Typechecker/Typechecker.java
+++ src/main/java/com/jamey/compiler/Typechecker/Typechecker.java
@@ -30,10 +30,11 @@ public class Typechecker {
     }
 
     public static Type typecheckBin(final BinaryExp exp,
-                                    final Map<Variable, Type> typeEnv)
+                                    final Map<Variable, Type> typeEnv,
+                                    final Optional<ClassType> inClass)
                                     throws TypecheckerErrorException{
-        final Type left = typecheckExp(exp.l(), typeEnv);
-        final Type right = typecheckExp(exp.r(), typeEnv);
+        final Type left = typecheckExp(exp.l(), typeEnv, inClass);
+        final Type right = typecheckExp(exp.r(), typeEnv, inClass);
         if((exp.op() instanceof PlusOp ||
             exp.op() instanceof MinusOp ||
             exp.op() instanceof MultOp ||
@@ -57,7 +58,8 @@ public class Typechecker {
      * ThisExp
      */
     public static Type typecheckExp(final Exp exp, 
-                                    final Map<Variable, Type> typeEnv)
+                                    final Map<Variable, Type> typeEnv,
+                                    final Optional<ClassType> inClass)
                                      throws TypecheckerErrorException{
         if(exp instanceof IntExp){
             return new IntType();
@@ -71,16 +73,23 @@ public class Typechecker {
                 throw new TypecheckerErrorException("Variable not in scope: " + variable.toString());
             }
         }else if(exp instanceof BinaryExp){
-            return typecheckBin((BinaryExp)exp, typeEnv);
+            return typecheckBin((BinaryExp)exp, typeEnv, inClass);
         }else if(exp instanceof ParenExp){
             ParenExp paren = (ParenExp)exp;
-            return typecheckExp(paren.e(), typeEnv);
+            return typecheckExp(paren.e(), typeEnv, inClass);
         }else if(exp instanceof PrintlnExp){
             /*
              * Might need to change this depending on what I can print
              */
             PrintlnExp print = (PrintlnExp)exp;
-            return typecheckExp(print.e(), typeEnv);
+            return typecheckExp(print.e(), typeEnv, inClass);
+        }else if(exp instanceof ThisExp){
+            if(inClass.isPresent()){
+                ClassType classType = inClass.get();
+                return classType;
+            }else{
+                throw new TypecheckerErrorException("Using 'this' when not in a class");
+            }
         }else if(exp instanceof NewExp){
             NewExp newExp = (NewExp)exp;
             String classname = newExp.type().name();
@@ -96,7 +105,7 @@ public class Typechecker {
                     throw new TypecheckerErrorException("Wrong number of arguments for constructor of '" + classname + "'");
                 }
                 for(int i = 0; i < constructorVardecs.size(); i++){
-                    Type arg = typecheckExp(argList.get(i), typeEnv);
+                    Type arg = typecheckExp(argList.get(i), typeEnv, inClass);
                     Type conVar = constructorVardecs.get(i).type();
                     if(!conVar.equals(arg)){
                         throw new TypecheckerErrorException("Initializing argument doesn't match type in the constructor");
@@ -109,12 +118,32 @@ public class Typechecker {
                 }
             }
             return newExp.type();
+        }else if(exp instanceof MethodCallExp method){
+            /*1. get the object type
+             * 2.check to see if the method is present within the class
+             * 3.can have multple method calls
+             * 4. final type
+             */
+            return typecheckMethodCall(method, typeEnv, inClass);
+
         }
         else{
             assert(false);
             throw new TypecheckerErrorException("Unrecognized expression: " + exp.toString());
         }
     }
+    public static Type typecheckMethodCall(final MethodCallExp exp, 
+                                           final Map<Variable, Type> typeEnv,
+                                           final Optional<ClassType> inClass)
+                                           throws TypecheckerErrorException{
+        Type objectType = typecheckExp(exp.e(), typeEnv, inClass);
+        if(!(objectType instanceof ClassType)){
+            throw new TypecheckerErrorException("Calling a method on object that isn't a class");
+        }
+        ClassType objectAsClassType = (ClassType)objectType;
+        exp.
+        return null;
+     }
 
     public static void assertTypesEqual(final Type expected, final Type received) throws TypecheckerErrorException{
         if(!(expected.equals(received))){
@@ -131,21 +160,23 @@ public class Typechecker {
         return addToMap(typeEnv, variable, type);
     }
     public static Map<Variable, Type> typecheckWhile(final WhileStmt stmt,
-                                                     final Map<Variable, Type> typeEnv) 
+                                                     final Map<Variable, Type> typeEnv,
+                                                     final Optional<ClassType> inClass) 
                                                      throws TypecheckerErrorException {
-        assertTypesEqual(new BoolType(), typecheckExp(stmt.e(), typeEnv));
+        assertTypesEqual(new BoolType(), typecheckExp(stmt.e(), typeEnv, inClass));
         for(Stmt body : stmt.stmt()){
-            typecheckStmt(body, typeEnv);
+            typecheckStmt(body, typeEnv, inClass);
         }
         return typeEnv;
     }
     public static Map<Variable, Type> typecheckAssign(final AssignStmt stmt,
-                                                      final Map<Variable, Type> typeEnv)
+                                                      final Map<Variable, Type> typeEnv,
+                                                      final Optional<ClassType> inClass)
                                                       throws TypecheckerErrorException{
         Variable variable = new Variable(stmt.name());
         if(typeEnv.containsKey(variable)){
             final Type expected = typeEnv.get(variable);
-            assertTypesEqual(expected, typecheckExp(stmt.e(), typeEnv));
+            assertTypesEqual(expected, typecheckExp(stmt.e(), typeEnv, inClass));
             return typeEnv;
         }else {
             throw new TypecheckerErrorException("Variable not in scope: " + stmt.name());
@@ -163,17 +194,18 @@ public class Typechecker {
      * 
      */
     public static Map<Variable, Type> typecheckStmt(final Stmt stmt,
-                                    final Map<Variable, Type> typeEnv)
+                                    final Map<Variable, Type> typeEnv,
+                                    final Optional<ClassType> inClass)
                                     throws TypecheckerErrorException{
         if(stmt instanceof VardecStmt){
             return typecheckVardec((VardecStmt)stmt, typeEnv);
         }else if(stmt instanceof WhileStmt){
-            return typecheckWhile((WhileStmt)stmt, typeEnv);
+            return typecheckWhile((WhileStmt)stmt, typeEnv, inClass);
         }else if(stmt instanceof AssignStmt){
-            return typecheckAssign((AssignStmt)stmt, typeEnv);
+            return typecheckAssign((AssignStmt)stmt, typeEnv, inClass);
         }else if(stmt instanceof ExpStmt){
             ExpStmt expstmt = (ExpStmt)stmt;
-            typecheckExp(expstmt.e(), typeEnv);
+            typecheckExp(expstmt.e(), typeEnv, inClass);
             return typeEnv;
         }
         else {
@@ -183,6 +215,7 @@ public class Typechecker {
 
     //for now this only checks the stmts and not all of the class definitions
     public static void typecheckProgram(final Program program) throws TypecheckerErrorException{
+
         Map<Variable, Type> typeEnv = new HashMap<Variable, Type>();
         for(final Stmt stmt : program.stmts){
             typeEnv = typecheckStmt(stmt, typeEnv);
